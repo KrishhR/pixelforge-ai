b6afab6be28edc73d43efe54268459fd94284d59
Krishnam Rastogi <rastogikrishnam00@gmail.com>
2025-12-09 01:13:06 +0530

    completed landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,43 @@
+import FeatureSection from '@/components/FeatureSection';
+import HeroSection from '@/components/Hero';
+import Pricing from '@/components/PricingSection';
+import StatsSection from '@/components/StatsSection';
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
+
 export default function Home() {
-    return <div></div>;
+    return (
+        <div className="pt-36">
+            {/* hero */}
+            <HeroSection />
+            {/* stats */}
+            <StatsSection />
+            {/* features */}
+            <FeatureSection />
+            {/* pricing */}
+            <Pricing />
+            {/* cta */}
+            {/* footer */}
+
+            <section className="text-center py-20">
+                <div className="max-w-4xl mx-auto px-6">
+                    <h2 className="text-5xl font-bold mb-6">
+                        Ready To{' '}
+                        <span className="bg-linear-to-r from-blue-400 via-purple-500 to-cyan-400 bg-clip-text text-transparent">
+                            Create Something Amazing?
+                        </span>
+                    </h2>
+                    <p className="text-xl text-gray-300 mb-8">
+                        Join thousands of creaters who are already using AI to transform their
+                        images and bring their vision to life.
+                    </p>
+                    <Link href="/dashboard">
+                        <Button variant="primary" size="xl">
+                            🌟 Start Creating Now
+                        </Button>
+                    </Link>
+                </div>
+            </section>
+        </div>
+    );
 }
---
+++ components/PricingCard.tsx
@@ -0,0 +1,104 @@
+import useIntersectionObserver from '@/hooks/useIntersectionObserver';
+import { SignedIn, SignedOut, useAuth } from '@clerk/nextjs';
+import { useState } from 'react';
+import { Button } from './ui/button';
+import { CheckoutButton } from '@clerk/nextjs/experimental';
+import { toast } from 'sonner';
+import { PlansType } from './PricingSection';
+
+const PricingCard = ({
+    id,
+    plan,
+    price,
+    features,
+    featured = false,
+    buttonText,
+    planId,
+}: PlansType) => {
+    const [ref, isVisible] = useIntersectionObserver();
+    const [isHovered, setIsHovered] = useState(false);
+
+    const { has } = useAuth();
+
+    const isCurrentPlan = id ? has?.({ plan: id }) : false;
+
+    const handlePopup = () => {
+        if (isCurrentPlan) return;
+
+        try {
+            // TODO_1 => handle "Upgrade to pro" button when user is not logged in!
+            // TODO_2 => bit of UI fixes
+        } catch (error) {
+            console.log('Checkout Error', error);
+            toast.error('Something went wrong' + error);
+        }
+    };
+
+    return (
+        <div
+            ref={ref}
+            className={`relative backdrop-blur-lg  border  rounded-3xl p-8 transition-all duration-700 cursor-pointer 
+                ${isVisible ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-10'}
+                ${isHovered ? 'transform scale-105 rotate-1 z-10' : ''}
+                ${
+                    featured
+                        ? 'bg-linear-to-b from-blue-500/20 to-purple-600/20 border-blue-400/50 scale-105'
+                        : 'bg-white/5 border-white/10'
+                }
+                `}
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}
+        >
+            {featured && (
+                <div className="absolute -top-4 left-1/2 transform -translate-x-1/2">
+                    <div className="bg-linear-to-r from-blue-500 to-purple-600 text-white px-6 py-2 rounded-full text-sm font-bold">
+                        Most Popular
+                    </div>
+                </div>
+            )}
+
+            <div className="text-center">
+                <h3 className="text-2xl font-bold text-white mb-2">{plan}</h3>
+                <div className="text-4xl font-bold bg-linear-to-r from-cyan-400 to-blue-500 bg-clip-text text-transparent mb-6">
+                    ${price} {price > 0 && <span className="text-lg text-gray-400">/month</span>}
+                </div>
+
+                <ul className="space-y-3 mb-8">
+                    {features.map((feature, idx) => (
+                        <li key={idx} className="flex items-center text-gray-300">
+                            <span className="text-green-400 mr-3">✓</span>
+                            {feature}
+                        </li>
+                    ))}
+                </ul>
+
+                <SignedIn>
+                    <CheckoutButton planId={planId!} planPeriod="month" for="user">
+                        <Button
+                            variant={featured ? 'primary' : 'glass'}
+                            size="xl"
+                            className="w-full"
+                            disabled={isCurrentPlan || !planId}
+                        >
+                            {isCurrentPlan ? 'Current Plan' : buttonText}
+                        </Button>
+                    </CheckoutButton>
+                </SignedIn>
+
+                <SignedOut>
+                    <Button
+                        onClick={handlePopup}
+                        variant={featured ? 'primary' : 'glass'}
+                        size="xl"
+                        className="w-full"
+                        disabled={isCurrentPlan || !planId}
+                    >
+                        {isCurrentPlan ? 'Current Plan' : buttonText}
+                    </Button>
+                </SignedOut>
+            </div>
+        </div>
+    );
+};
+
+export default PricingCard;
